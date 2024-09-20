022e68ef18de867ddd672a0e7db14a2016e87e67
Isaac Rosado <irosado@agl.com.au>
2024-09-20 11:39:04 +1000

    feat: client cert

--- server/src/main/kotlin/com/espero/yaade/model/db/CertificateDb.kt
+++ server/src/main/kotlin/com/espero/yaade/model/db/CertificateDb.kt
@@ -76,12 +76,10 @@ class CertificateDb {
                     ?: return
                 val pemKey = certData.getJsonObject("pemConfig")?.getString("key")
                     ?: return
-                webClientOptions.trustOptions = PemTrustOptions()
-                    .addCertValue(Buffer.buffer(pemCert))
-                webClientOptions.keyCertOptions = PemKeyCertOptions()
-                    .addCertValue(Buffer.buffer(pemCert))
-                    .addKeyValue(Buffer.buffer(pemKey))
-                webClientOptions.setSsl(true)
+                webClientOptions.setPemKeyCertOptions = PemKeyCertOptions()
+                    .setCertValue(Buffer.buffer(pemCert))
+                    .setKeyValue(Buffer.buffer(pemKey))
+                webClientOptions.setSsl(true).setTrustAll(true)
             }
         }
     }
